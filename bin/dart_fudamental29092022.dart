4b6a5b26d11b75b2886cb8b68762631c65a882a6
Akatsuki666 <nguyentruongphi15032003@gmail.com>
2022-10-04 18:58:10 +0700

    Initial commit

---
+++ bin/dart_fudamental29092022.dart
@@ -0,0 +1,27 @@
+import 'package:dart_fudamental29092022/dart_fudamental29092022.dart' as dart_fudamental29092022;
+
+void main(List<String> arguments) {
+
+  // 1. Kiểu dữ liễu
+  // Kiểu số
+  // num weight = 75.5;
+
+  // Kiểu chuỗi
+  // String name = "phi";
+
+  // Kiểu luận lý
+  // bool isMate = true;
+
+  // Kiểu run
+  // String unicode1 = "\u01FF";
+  // print(unicode1);
+
+  // Runes runes = Runes(
+  //   '\u{2651} \u{2652}'
+  // );
+  // print(runes);
+
+  dynamic a = 10;
+  a = "11";
+  print(a);
+}
